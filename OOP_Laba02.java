1ee5e4f478aa803c76b2a77c0b022deaa120f79c
NazarFilosoF <104684848+NazarFilosoF@users.noreply.github.com>
2026-09-14 13:56:08 +0300

    Dodelat

---
+++ OOP_Laba02.java
@@ -0,0 +1,59 @@
+public class OOP_Laba01 {
+    public static class Book {
+        private long id;
+        private String title;
+        private String author;
+        private int year;
+        private static int odjcount = 0;
+        // public Book(long id, String title, String author, int year){ Это без автоинкремента!!
+        //     this.id = id;
+        //     this.title = title;
+        //     this.author = author;
+        //     this.year = year;
+        // }
+        public Book(String title, String author, int year){
+            this.id = ++objcount;
+            this.title = title;
+            this.author = author;
+            this.year = year;
+        }
+        public Book(){
+            this("Без названия", "Не указан", 0);
+        }
+        public Book(String title, String author) {
+            this(title, author, 0);
+        }
+        public static int getCount(){
+            return objcount;
+        }
+        public void setCount(int objcount){
+            this.objcount = objcount;
+        }
+        public void setTitle(String title){
+            if (title == null || title.isBlank()) throw new IllegalArgumentException("Название не может быть пустым!");
+            this.title = title;
+        }
+        public void setAuthor(String author){
+            if (author == null || author.isBlank()) throw new IllegalArgumentException("Автор не может быть пустым!");
+            this.author = author;
+        }
+        public void setYear(int year){
+            if (year < 0 || year > java.time.Year.now().getValue()) throw new IllegalArgumentException("Название не может быть пустым!");
+            this.year = year;
+        }
+        public long getId(){
+            return id;
+        }
+        public String getDescription(){
+            return "\"" + title + "\" - " + author + " (" + year + ")";
+        }
+    }
+    public static void main(String[] args) {
+        Book firstbook = new Book("Война и мир", "Толстой Л.Н.", 1869);
+        Book secondbook = new Book("Мастер и Маргарита", "Булгаков М.А.", 1967);
+        Book thirdbook = new Book("Преступление и наказание", "Достоевский Ф.М.", 1866);
+        System.out.println(firstbook.getId() + "." + " " + firstbook.getDescription());
+        System.out.println(secondbook.getId() + "." + " " + secondbook.getDescription());
+        System.out.println(thirdbook.getId() + "." + " " + thirdbook.getDescription());
+    }
+}
