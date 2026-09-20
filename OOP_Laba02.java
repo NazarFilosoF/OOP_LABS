0e668ac67468218896dee37c0aba5a89803d4879
NazarFilosoF <104684848+NazarFilosoF@users.noreply.github.com>
2026-09-20 14:49:36 +0300

    Without Autoincrement!!

--- OOP_Laba02.java
+++ OOP_Laba02.java
@@ -4,30 +4,33 @@ public class OOP_Laba01 {
         private String title;
         private String author;
         private int year;
-        private static int odjcount = 0;
-        // public Book(long id, String title, String author, int year){ Это без автоинкремента!!
-        //     this.id = id;
-        //     this.title = title;
-        //     this.author = author;
-        //     this.year = year;
-        // }
-        public Book(String title, String author, int year){
-            this.id = ++objcount;
+        private static int objcount = 0;
+        public Book(long id, String title, String author, int year){ 
+            this.id = id;
             this.title = title;
             this.author = author;
             this.year = year;
         }
+        // public Book(String title, String author, int year){
+        //     this.id = ++objcount;
+        //     this.title = title;
+        //     this.author = author;
+        //     this.year = year;
+        // }
         public Book(){
-            this("Без названия", "Не указан", 0);
+            this(0, "Без названия", "Не указан", 0);
+        }
+        public Book(String title, String author, int year) {
+            this(0, title, author, year);
         }
         public Book(String title, String author) {
-            this(title, author, 0);
+            this(0, title, author, 0);
         }
         public static int getCount(){
             return objcount;
         }
-        public void setCount(int objcount){
-            this.objcount = objcount;
+        public static void setCount(int objcount){
+            Book.objcount = objcount;
         }
         public void setTitle(String title){
             if (title == null || title.isBlank()) throw new IllegalArgumentException("Название не может быть пустым!");
@@ -49,9 +52,9 @@ public class OOP_Laba01 {
         }
     }
     public static void main(String[] args) {
-        Book firstbook = new Book("Война и мир", "Толстой Л.Н.", 1869);
-        Book secondbook = new Book("Мастер и Маргарита", "Булгаков М.А.", 1967);
-        Book thirdbook = new Book("Преступление и наказание", "Достоевский Ф.М.", 1866);
+        Book firstbook = new Book(1, "Война и мир", "Толстой Л.Н.", 1869);
+        Book secondbook = new Book(2, "Мастер и Маргарита", "Булгаков М.А.", 1967);
+        Book thirdbook = new Book(3, "Преступление и наказание", "Достоевский Ф.М.", 1866);
         System.out.println(firstbook.getId() + "." + " " + firstbook.getDescription());
         System.out.println(secondbook.getId() + "." + " " + secondbook.getDescription());
         System.out.println(thirdbook.getId() + "." + " " + thirdbook.getDescription());
